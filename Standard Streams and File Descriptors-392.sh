d5c8962766ce1b1e3f705938268f7004ea912fd7
Rohini Joshi <78068423+Rohini2112@users.noreply.github.com>
2022-07-18 15:47:27 -0500

    Add files via upload

---
+++ Standard Streams and File Descriptors-392.sh
@@ -0,0 +1,35 @@
+## 1. Standard Streams ##
+
+/home/dq$ ls /home/inexistent 2>>stderr
+
+## 2. Recap ##
+
+/home/dq$ cmd1 | cmd2
+
+## 3. Redirecting Two Streams ##
+
+/home/dq$ cat all_output
+
+## 4. File Descriptors ##
+
+/home/dq$ date
+
+## 5. A New Point of View ##
+
+/home/dq$ date
+
+## 6. Duplicating File Descriptors ##
+
+/home/dq$ echo "The first clue is in an image you encountered in this cou
+
+## 7. Order of Redirections ##
+
+/home/dq$ diff -y redirection_order order_verification
+
+## 8. Standard Input ##
+
+/home/dq$ cat sorted_stdin
+
+## 9. Redirecting Standard Input ##
+
+/home/dq$ tr [:lower:] [:upper:] <sorted_stdin >mad_vowels
